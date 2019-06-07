8460fc8b9a0b02354369e05fc6e99b9753b507d8
Cosmic0wl <valentinaspanetta@gmail.com>
2019-06-07 12:59:06 +0200

    button function

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,18 +5,38 @@ $(document).ready(function() {
         var myImage = $('<img src=' + obj[i].img + '>');
         var imgDiv = $(".image");
         $(imgDiv[i]).append(myImage);
-        $("img").addClass("img-fluid");
         var myText = $('<p>' + obj[i].title + '</p>' + '<p>' + obj[i].description + '</p>');
         var textDiv = $(".text");
         $(textDiv[i]).append(myText);
-        $("p").addClass("text-light");
         var myLikes = $('<span>' + obj[i].likes + '</span>');
         var likesDiv = $(".likes");
         $(likesDiv[i]).append(myLikes);
         }
 
      // LIKE BUTTON FUNCTION
+     var btn = $(".button");
+     var obj = movies;
+     var i = 0;
+     for (let i = 0; i < btn.length; i++) {
+     	$(btn[i]).click(function() {
+     		incrementLikes(i);
+     	});
+     }
+
+     
+     function incrementLikes(i) {
+     	var obj = movies;
+     	counter = obj[i].likes;
+     	counter++;
+     	return counter;
+     }
 
 
-     // CLASSES
+     // SORT FUNCTION
+
+
+     // STYILING
+     $("p, span").addClass("text-light");
+     $("img").addClass("img-fluid");
+
 });
